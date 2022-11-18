259cac75b13b41dc49b92bafddd0e5ee5b28a16f
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-18 13:25:41 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,18 @@
-import {  NotificationSpan } from './AppStyle.js';
-import Notification from './Notification/Notification';
+// import {  NotificationSpan } from './AppStyle.js';
+// import Notification from './Notification/Notification';
 // import ContactList from './ContactList/ContactList';
 // import ContactForm from './ContactForm/ContactForm';
+import SearchBar from './SearchBar/SearchBar';
 
-const contacts =  [
-  { id: 0, text: "Learn HTML and CSS", completed: true },
-  { id: 1, text: "Get good at JavaScript", completed: true },
-  { id: 2, text: "Master React", completed: false },
-  { id: 3, text: "Discover Redux", completed: false },
-  { id: 4, text: "Build amazing apps", completed: false },
-]
+
 
 export const App = () => {
 
 
 
-  const filteredContacts = contacts;
+
   return (
-    <>
-      {filteredContacts.length > 0 ? (
-        {/* <ContactList /> */}
-      ) : (
-        <NotificationSpan>
-          <Notification message="No contacts yet" />
-        </NotificationSpan>
-      )}
-    </>
+    <SearchBar />
   );
 };
 
