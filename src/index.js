26c4e142ac20f537d056834fde71729146cf8a2a
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-18 13:23:58 +0300

    create SearchBar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import './index.css';
 
+const basename =
+  process.env.NODE_ENV !== 'production'
+    ? '/goit-react-hw-06-phonebook'
+    : '/goit-react-hw-06-phonebook/';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <BrowserRouter basename={basename}>
   <Provider store={store}>
     <App />
   </Provider>
+  </BrowserRouter>
+</React.StrictMode>
+ 
 );
