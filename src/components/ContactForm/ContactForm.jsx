01b98f65ac778436b988711ba3d64303fcecc968
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-19 11:18:40 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getItems } from '../../redux/tasksSlice';
 import Notiflix from 'notiflix';
 import { nanoid } from 'nanoid';
-import { addContact } from "../../redux/tasksSlice";
+import { addContact } from '../../redux/tasksSlice';
 
 import {
   InputLabel,
@@ -15,37 +15,35 @@ import {
 } from './ContactFormStyle.js';
 
 const ContactForm = () => {
-
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
   const items = useSelector(getItems);
   const dispatch = useDispatch();
 
-  const handleChange = (event) => {
+  const handleChange = event => {
     const { name, value } = event.target;
     if (name === 'name') {
-        setName(value);
-    } else if (name === 'number') {
-        setNumber(value);
+    } else if (value === 'number') {
     }
-}
+  };
 
-const handleSubmitForm = (event) => {
-  if (items.find(item => item.name === name)) {
-    Notiflix.Notify.failure(`This contact is already exists`, { position: "top-center", autoClose: 5000 } );
-         return;
-     }
-     event.preventDefault();
-  dispatch(addContact({ id: nanoid(), name, number }));
-  reset();
- }
+  const handleSubmitForm = event => {
+    if (items.find(item => item.name === name)) {
+      Notiflix.Notify.failure(`This contact is already exists`, {
+        position: 'top-center',
+        autoClose: 5000,
+      });
+      return;
+    }
+    event.preventDefault();
+    dispatch(addContact({ id: nanoid(), name, number }));
+    reset();
+  };
 
- const reset = () => {
-  setName('')
-  setNumber('')
-};
-
-
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
 
   return (
     <FormWrap onSubmit={handleSubmitForm}>
@@ -55,9 +53,6 @@ const handleSubmitForm = (event) => {
           onChange={handleChange}
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
         />
       </InputLabel>
       <InputLabel>
@@ -66,9 +61,6 @@ const handleSubmitForm = (event) => {
           onChange={handleChange}
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
         />
       </InputLabel>
       <FormBtn type="submit">Add contact</FormBtn>
