4d3116c38b3858cba2b688bee2a3e6ae874694ab
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-19 11:32:01 +0300

    bag fix

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
-import { store, persistor } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store} from './redux/store';
 import { Provider } from 'react-redux';
 import './index.css';
 
@@ -16,9 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename={basename}>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
+      <App />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
