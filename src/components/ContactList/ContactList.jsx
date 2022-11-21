923ca1a6a6b48ff80ff9cec30e330533794adfc7
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-21 12:46:44 +0300

    divided into components

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,20 @@
 import { useSelector } from 'react-redux';
 import { getFilter, getItems } from '../../redux/tasksSlice';
-import { ContactListItem } from '../ContactListItem/ContactListItem';
-
+import { List } from './ContactList.styled';
+import { ContactListItem } from '../../components/ContactListItem/ContactListItem'
 
 export const ContactList = () => {
   const items = useSelector(getItems);
   const filter = useSelector(getFilter);
-  const filteredItems = items.filter(item => 
-    item.name.toLowerCase().includes(filter.toLowerCase()));
-    return (
-      <ul>
-        {filteredItems.map(item => (
-          <ContactListItem key={item.id} item={item} />
-        ))}
-      </ul>
-    );
-
-}
+  const filteredItems = items.filter(item =>
+    item.name.toLowerCase().includes(filter.toLowerCase())
+  );
+  return (
+    <List>
+      {filteredItems.map(item => (
+        <ContactListItem key={item.id} item={item} />
+      ))}
+    </List>
+  );
+};
 export default ContactList;
--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,27 +1,27 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/tasksSlice';
-import { ContactListItemContainer } from './ContactListItem.styled';
+import { SpanName, SpanNumber, ContactBtn, ContactItem } from './ContactListItem.styled';
 
 export const ContactListItem = ({ item: { id, number, name } }) => {
-    const dispatch = useDispatch();
-    return (    
-        <li>
-            <ContactListItemContainer>
-                    <p>{name}</p>
-                    <p>{number}</p>
-                <button type='button' onClick={() => dispatch(deleteContact(id))}>Delete</button>
-            </ContactListItemContainer>
-        </li>
-        )
-    }
+  const dispatch = useDispatch();
+  return (
+    <ContactItem>
+      <SpanName>{name}</SpanName>
+      <SpanNumber>{number}</SpanNumber>
+      <ContactBtn type="button" onClick={() => dispatch(deleteContact(id))}>
+        Delete
+      </ContactBtn>
+    </ContactItem>
+  );
+};
 
- export default ContactListItem;  
+export default ContactListItem;
 
- ContactListItem.propTypes = {
-    contacts: PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    }),
-  };
+// ContactListItem.propTypes = {
+//   contacts: PropTypes.shape({
+//     id: PropTypes.string.isRequired,
+//     name: PropTypes.string.isRequired,
+//     number: PropTypes.string.isRequired,
+//   }),
+// };
