0aad2d06326a0257707d2747c8b208735cdd4efb
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-21 13:37:35 +0300

    tweaked the styles

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,18 +1,18 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { filteredContacts, getFilter } from '../../redux/tasksSlice';
-import { Label } from './Filter.styled';
+import { Label,FilterInput, FilterWrap } from './Filter.styled';
 
 const Filter = () => {
   const filter = useSelector(getFilter);
   const dispatch = useDispatch();
   return (
-    <div>
+    <FilterWrap>
       <Label htmlFor="filter">Find contacts by name</Label>
-      <input
+      <FilterInput
         value={filter}
         onChange={event => dispatch(filteredContacts(event.target.value))}
       />
-    </div>
+    </FilterWrap>
   );
 };
 
