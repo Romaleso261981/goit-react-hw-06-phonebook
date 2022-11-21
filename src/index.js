47a023915c0b19a3c4a6883000c0d972efe75484
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-21 13:38:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
 import { store} from './redux/store';
 import { Provider } from 'react-redux';
 import './index.css';
 
-const basename =
-  process.env.NODE_ENV !== 'production'
-    ? '/goit-react-hw-06-phonebook'
-    : '/goit-react-hw-06-phonebook/';
+// const basename =
+//   process.env.NODE_ENV !== 'production'
+//     ? '/goit-react-hw-06-phonebook'
+//     : '/goit-react-hw-06-phonebook/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
-      <Provider store={store}>
+   <Provider store={store}>
       <App />
       </Provider>
-    </BrowserRouter>
   </React.StrictMode>
 );
