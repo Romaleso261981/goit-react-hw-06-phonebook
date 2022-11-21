6f82b19ece662c87f6e3c20a2dcd9751730244f4
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-21 13:49:44 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
-import { store} from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import './index.css';
 
@@ -14,7 +15,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <Provider store={store}>
-      <App />
+   <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
       </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -23,6 +23,3 @@ export const store = configureStore({
 });
 
 export const persistor = persistStore(store);
-
-
-
